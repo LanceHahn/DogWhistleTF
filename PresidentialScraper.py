b1eb656dc62d2b9dba630eef1e0d080333d93e80
Lance Hahn <dr.lwhahn@gmail.com>
2022-10-16 21:29:08 -0500

    first commit

---
+++ PresidentialScraper.py
@@ -0,0 +1,165 @@
+import platform
+
+import urllib.request
+
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+import urllib.request
+import warnings
+import re
+
+from bs4 import BeautifulSoup
+import ssl
+
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class PresidentialScraper:
+    def __init__(self, link):
+        self.pages = []
+        self.corpus = ""
+
+        page = urllib.request.urlopen(link)
+        soup = BeautifulSoup(page, "html.parser")
+
+        pageCount = 0
+        while page is not None:
+            print(f"Processing query page {pageCount}")
+            pageCount += 1
+            evens = soup.select(".even")
+            odds = soup.select(".odd")
+            for i in odds:
+                evens.append(i)
+            for i in evens:
+                link = "https://www.presidency.ucsb.edu" + i.select('a')[1]['href']
+                self.pages.append(urllib.request.urlopen(link))
+            try:
+                page = urllib.request.urlopen("https://www.presidency.ucsb.edu" +
+                                              soup.find('a', {'title': 'Go to next page'})['href'])
+                soup = BeautifulSoup(page, "html.parser")
+            except TypeError:
+                break
+
+    def create_corpus(self):
+        print(f"create corpus from {len(self.pages)} pages")
+
+        for i in self.pages:
+            self.corpus += extract_speech(i)
+        self.corpus = self.corpus.lower()
+
+        replace_dict = {
+            '[': "",
+            ']': "",
+            "(": "",
+            ")": "",
+            # ".": "",
+            "mr.": "mr",
+            "mrs.": "mrs",
+            "ms.": "ms",
+            ",": "",
+            "!": "",
+            "#": "",
+            ":": "",
+            "@": "",
+            "?": "",
+            "they're": "they are",
+            "what's": "what is",
+            "let's": "let us",
+            "there've": "there have",
+            "i've": "i have",
+            "don't": "do not",
+            "we'd": "we had",
+            "he'd": "he had",
+            "we're": "we are",
+            "they're": "they are",
+            "is'nt": "is not",
+            "that's": "that is",
+            "it's": "it is",
+            "he's": "he is",
+            "wasn't": "was not",
+            "i'd": "i would",
+            "you've": "you have",
+            "you're": "you are",
+            "you'd": "you would",
+            "i'm": "i am",
+            "we've": "we have",
+            '-': " ",
+            '"': "",
+            "&": ""
+        }
+        # need to remove periods that are not ending a sentence like "Mr. Trump"
+        stop = stopwords.words("english")
+        self.corpus = multiple_replace(replace_dict, self.corpus)
+        self.corpusSentenceTokens = [[token for token in nltk.word_tokenize(sent)
+                                      if token not in stop]
+                                     for sent in self.corpus.split('.')]
+        self.corpus = [token for token in nltk.word_tokenize(self.corpus) if token != '.']
+        originalLength = len(self.corpus)
+        self.corpus = [word for word in self.corpus if word not in stop]
+        print(f"Removing stop words reduced the {originalLength}-word "
+              f"corpus by {originalLength - len(self.corpus)} to {len(self.corpus)} words.")
+        temp = [self.corpus]
+        self.corpus = temp
+        print(f"corpus of {len(self.corpus[0])} tokenized words.")
+        print(f"corpus of {len(self.corpusSentenceTokens)} tokenized sentences "
+              f"containing {sum(len(sent) for sent in self.corpusSentenceTokens)} words.")
+        return
+
+
+def extract_speech(page):
+    full_text = ""
+    soup = BeautifulSoup(page, "html.parser")
+    if "Tweets" in soup.select_one("title").getText():
+        tweets = soup.select("td")
+        for num, tweet in enumerate(tweets):
+            # go through td elements and add only odd ones due to indexing
+            if num % 2 != 0:
+                temp = tweet.getText()
+                temp = temp.split("Retweets", 1)
+                full_text += temp[0] + " "
+
+        full_text = multiple_replace({"RT": "", "@": ""}, full_text)
+    else:
+        article = soup.select(".field-docs-content")
+        for i in article:
+            temp = i.getText()
+            full_text += temp
+
+    return full_text
+
+
+# Function used is from ActiveState
+# https://code.activestate.com/recipes/81330-single-pass-multiple-replace/
+def multiple_replace(dict, text):
+    # Create a regular expression  from the dictionary keys
+    regex = re.compile("(%s)" % "|".join(map(re.escape, dict.keys())))
+    # For each match, look-up corresponding value in dictionary
+    return regex.sub(lambda mo: dict[mo.string[mo.start():mo.end()]], text)
+
+
+def showSample(vectors, target, count=2, modelLabel=""):
+    if target in vectors.key_to_index.keys():
+        sims = vectors.most_similar(target)[:2]
+        if len(sims) >= count:
+            print(f"{count} similar words for '{target}' using the {modelLabel}: {sims}")
+            print(f"with {modelLabel} vocab size {len(vectors.key_to_index)}")
+        else:
+            print(f"{target} did not produce {count} similar words.")
+    else:
+        print(f"{target} is not available in the vocabulary.")
+    return
+
+
+if __name__ == '__main__':
+    TrumpList = PresidentialScraper("https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2="
+                                    "&field-keywords3=&from%5Bdate%5D=&to%5Bdate%5D=&person2=200301&category2%5B%5D="
+                                    "83&items_per_page=100")
+    TrumpList.create_corpus()
+    cleaned_sentences = TrumpList.corpusSentenceTokens # corpus
+
+
+    print(f" vocabulary using {sum(len(sent) for sent in cleaned_sentences)} words "
+          f"in {len(cleaned_sentences)} sentences.")
---
+++ preTrainedSample.py
@@ -0,0 +1,244 @@
+"""
+Originally inspired by
+https://keras.io/examples/nlp/pretrained_word_embeddings/
+"""
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.layers import TextVectorization
+
+import os
+import pathlib
+
+
+def acquireData(dataDir):
+    """
+    generate a list of labels and sample data by processing the path provided
+    :param dataDir: the path to a directory with directories of documents where
+    the subdirectory is treated as the label and the documents are sample data
+    :return: list of labels, list of data
+    """
+    print(f"Reading in training and testing data from {dataDir}")
+    dirnames = os.listdir(dataDir)
+    print("We've created an access point to some newsgroup data. \nSummary Info:")
+    print("Number of directories:", len(dirnames))
+    print("Directory names:", dirnames)
+
+    fnames = os.listdir(dataDir / "comp.graphics")
+    print("As an example, here are the number of files in one directory named `comp.graphics`:", len(fnames))
+    print("Some example filenames from 'comp.graphics':", fnames[:5])
+
+    sampleFile = dataDir / "comp.graphics" / "38987"
+    print(f"\nA sample file ({sampleFile}) contents:")
+    print(open(dataDir / "comp.graphics" / "38987").read())
+    print("Acquiring data as a list of document strings and a list of labels associated with the strings.")
+    print("The class name is the source discussion board and the label is the index")
+    samples = []
+    labels = []
+    class_names = []
+    class_index = 0
+    for dirname in sorted(os.listdir(data_dir)):
+        class_names.append(dirname)
+        dirpath = data_dir / dirname
+        fnames = os.listdir(dirpath)
+        print(f"Processing directory {dirname}, {len(fnames)} files will be associated "
+              f"with the label {dirname}")
+        for fname in fnames:
+            fpath = dirpath / fname
+            f = open(fpath, encoding="latin-1")
+            content = f.read()
+            lines = content.split("\n")
+            lines = lines[10:]  # skip header info in message
+            content = "\n".join(lines)  # recombine msg as single string
+            samples.append(content)  # list of training strings/documents
+            labels.append(class_index)   # label index associated with text
+        class_index += 1
+    print(f"Classes ({len(class_names)}):", class_names)
+    print("Number of samples:", len(samples))
+    return labels, samples
+
+
+def organizeData(labels, samples, batchSize=128, validSplit=0.2, mxVocab=20000, mxSentence=200):
+    """
+    Break the data into a training set and a validation set.  Also create
+    a vectorizer that can be used to convert a string into a list of indexes which
+    can be used to train and use the model
+    :param labels: the list of labels associated with the data samples
+    :param samples: the list of the data samples
+    :param batchSize: how many data samples are processed at one time
+    :param validSplit: the proportion of data to be used for validation
+    :param mxVocab: the size limit of the vocabulary
+    :param mxSentence: the number of tokens in a sentence
+    :return:
+    """
+    # Shuffle the data
+    seed = 1337
+    rng = np.random.RandomState(seed)
+    rng.shuffle(samples)
+    rng = np.random.RandomState(seed)
+    rng.shuffle(labels)
+
+    # Extract a training & validation split
+    num_validation_samples = int(validSplit * len(samples))
+    print(f"{validSplit} of dataset of {len(samples)} is divided into {len(samples)-num_validation_samples} training samples and"
+          f" {num_validation_samples} validation samples.")
+    train_samples = samples[:-num_validation_samples]
+    val_samples = samples[-num_validation_samples:]
+    train_labels = labels[:-num_validation_samples]
+    val_labels = labels[-num_validation_samples:]
+    print(f"{len(train_samples)} training samples")
+    print(f"{len(val_labels)} validation samples")
+    print(f"Vector representing a sentence is at most {mxSentence} tokens long.")
+    print(f"Vocabulary is the top {mxVocab} words.")
+    print(f"The training sample will be processed in batches containing {batchSize} values.")
+
+    print(f"Training vectors are {mxSentence}-dim representing single sentences.")
+    vectorizer = TextVectorization(max_tokens=mxVocab, output_sequence_length=mxSentence)
+    text_ds = tf.data.Dataset.from_tensor_slices(train_samples).batch(batchSize)
+    vectorizer.adapt(text_ds)
+
+    print(f"The top 5 words in vectorization: {vectorizer.get_vocabulary()[:5]}")
+    sampleText = "the cat sat on the mat"
+    output = vectorizer([[sampleText]])
+    print(f"The sentence '{sampleText}' converts into a vector of {len(output[0])} token indexes that is padded with 0s of:"
+          f"{output.numpy()[0, :6]}")
+    return train_samples, train_labels, val_samples, val_labels, vectorizer
+
+
+def loadEmbedding(vectorizer, embedFile):
+
+    voc = vectorizer.get_vocabulary()
+    word_index = dict(zip(voc, range(len(voc))))
+
+    print(f"Begin acquiring embedding vectors from {embedFile}")
+    #C:\Users\drlwh\OneDrive\Documents\GitHub\DogWhistleTF\glove.6B
+    embeddings_index = {}
+    with open(embedFile, encoding="utf8") as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, "f", sep=" ")
+            embeddings_index[word] = coefs
+    print(f"Found {len(embeddings_index)} word vectors. Each vector is {len(embeddings_index[word])} long.")
+
+    num_tokens = len(voc) + 2
+    hits = 0
+    misses = 0
+    # Prepare embedding matrix
+    embedding_matrix = np.zeros((num_tokens, embedding_dim))
+    missed = set()
+    for word, i in word_index.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # Words not found in embedding index will be all-zeros.
+            # This includes the representation for "padding" and "OOV"
+            embedding_matrix[i] = embedding_vector
+            hits += 1
+        else:
+            misses += 1
+            missed.add(word)
+    print("Converted %d words (%d misses)" % (hits, misses))
+    print("'misses' are words that didn't make it into our embedding space.")
+    print(f"Full list of misses: {missed}")
+    return embedding_matrix
+
+
+def designModel(embedMatrix, classCount, batchSize):
+    # Create the keras embedding layer and initialize it using our acquired embedding vectors
+    #from tensorflow.keras.layers import Embedding
+    from keras.layers import Embedding
+    voc = vectorizer.get_vocabulary()
+    num_tokens = len(voc) + 2
+    embedding_layer = Embedding(
+        num_tokens,
+        embedding_dim,
+        embeddings_initializer=keras.initializers.Constant(embedMatrix),
+        trainable=False,
+    )
+
+    # A simple 1D convnet with global max pooling and a classifier at the end.
+    windowSz = 5
+    dropProb = 0.5
+    print(f"A 3-layer Conv1D with {batchSize} input vectors having window size of {windowSz} ")
+    print("Global Max pooling looks across the input vector and takes the single largest value")
+    print(f"A Dropout layer with dropout probability of {dropProb} is near the end of the model.")
+
+
+    int_sequences_input = keras.Input(shape=(None,), dtype="int64")
+    embedded_sequences = embedding_layer(int_sequences_input)
+    x = layers.Conv1D(batchSize, windowSz, activation="relu")(embedded_sequences)
+    x = layers.MaxPooling1D(windowSz)(x)
+    x = layers.Conv1D(batchSize, windowSz, activation="relu")(x)
+    x = layers.MaxPooling1D(windowSz)(x)
+    x = layers.Conv1D(batchSize, windowSz, activation="relu")(x)
+    x = layers.GlobalMaxPooling1D()(x)
+    x = layers.Dense(batchSize, activation="relu")(x)
+    x = layers.Dropout(dropProb)(x)
+    preds = layers.Dense(classCount, activation="softmax")(x)
+    model = keras.Model(int_sequences_input, preds)
+    print("A summary description of our model.")
+    model.summary()
+    return model
+
+
+def trainModel(trainData, trainLabels, validData, validLabels, vectorizer, model):
+
+    x_train = vectorizer(np.array([[s] for s in trainData])).numpy()
+    x_val = vectorizer(np.array([[s] for s in validData])).numpy()
+
+    y_train = np.array(trainLabels)
+    y_val = np.array(validLabels)
+
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="rmsprop", metrics=["acc"]
+    )
+    model.fit(x_train, y_train, batch_size=128, epochs=20, validation_data=(x_val, y_val))
+    string_input = keras.Input(shape=(1,), dtype="string")
+    x = vectorizer(string_input)
+    preds = model(x)
+    end_to_end_model = keras.Model(string_input, preds)
+
+    return end_to_end_model
+
+
+def useModel(model, classLabels):
+
+    testText = "this message is about computer graphics and 3D modeling"
+    probabilities = model.predict([[testText]])
+    print(f"'{testText}' -> {classLabels[np.argmax(probabilities[0])]}")
+    print("Enter QUIT to stop being prompted for a phrase.")
+    while testText.lower() != 'quit':
+        testText = input("Phrase to categorize: ")
+        probabilities = model.predict([[testText]])
+        bestProbIX = np.argmax(probabilities[0])
+        bestClass = classLabels[bestProbIX]
+        print(f"'{testText}' is most associated with class '{bestClass}' with weight "
+              f"{probabilities[0][bestProbIX]}")
+    return
+
+
+if __name__ == '__main__':
+    embedding_dim = 100
+    batchSize = 128
+    data_path = keras.utils.get_file(
+        "news20.tar.gz",
+        "http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.tar.gz",
+        untar=True,
+    )
+    data_dir = pathlib.Path(data_path).parent / "20_newsgroup"
+    labels, data = acquireData(data_dir)
+    classLabels = [dirname for dirname in sorted(os.listdir(data_dir))]
+    classCount = len(list(set(labels)))
+
+    samplesTrain, labelsTrain, samplesValidate, labelsValidate, vectorizer = organizeData(labels, data, batchSize=128,
+                                                                                          validSplit=0.2,
+                                                                                          mxVocab=20000, mxSentence=200)
+    embedFileName = os.path.join(
+        os.path.expanduser("~"), "C:\\Users\\drlwh\\OneDrive\\Documents\\GitHub\\DogWhistleTF\\"
+                                 f"glove.6B\\glove.6B.{embedding_dim}d.txt"
+    )
+    embedSpace = loadEmbedding(vectorizer, embedFileName)
+    modelArch = designModel(embedSpace, len(classLabels), batchSize)
+    modelTrained = trainModel(samplesTrain, labelsTrain, samplesValidate, labelsValidate, vectorizer, modelArch)
+    useModel(modelTrained, classLabels)
