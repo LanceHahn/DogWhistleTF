4c6280b3cdad7588f323f526ae9321381f6ebde9
desmondharris <desmondktharris@gmail.com>
2022-10-26 14:46:45 -0500

    Fix bugs in writeData function

--- PartyClassifier.py
+++ PartyClassifier.py
@@ -5,6 +5,7 @@ from preTrainedSample import organizeData, loadEmbedding, designModel, trainMode
 import os
 import io
 
+
 def createData(labels, searches):
     """
     Create a dataset that can later be fed to organizeData(). Better for smaller datasets/testing
@@ -21,7 +22,7 @@ def createData(labels, searches):
     page_count = 0
 
     for label, search in zip(labels, searches):
-        search_page = urllib.request.urlopen(search)
+        search_page = urllib.request.urlopen(search, timeout=30)
         soup = BeautifulSoup(search_page, "html.parser")
         while search_page is not None:
             print(f"Processing query page {page_count}")
@@ -32,18 +33,18 @@ def createData(labels, searches):
                 evens.append(i)
             for article in evens:
                 samples.append(extract_speech(
-                    urllib.request.urlopen("https://www.presidency.ucsb.edu" + article.select('a')[1]['href'])))
+                    urllib.request.urlopen("https://www.presidency.ucsb.edu" + article.select('a')[1]['href'], timeout=30)))
                 classes.append(label)
             try:
                 page = urllib.request.urlopen("https://www.presidency.ucsb.edu" +
-                                              soup.find('a', {'title': 'Go to next page'})['href'])
+                                              soup.find('a', {'title': 'Go to next page'})['href'], timeout=30)
                 soup = BeautifulSoup(page, "html.parser")
             except TypeError:
                 break
     return classes, samples
 
 
-def writeData(labels, searches, new_folder_title):
+def writeData(labels, searches, max_files_each=20_000, new_folder_title="processed_data"):
     """
     Write given speeches to a specified directory.
 
@@ -51,6 +52,8 @@ def writeData(labels, searches, new_folder_title):
     :type labels: list
     :param searches: Ordered list of presidential archive links, which will be scraped and assigned labels
     :type searches: list
+    :param max_files_each: Max number of files to be processed for each class
+    :type max_files_each: int
     :param new_folder_title: Title of folder where you want to store the files
     :type new_folder_title: str
     :return: No return value
@@ -58,36 +61,40 @@ def writeData(labels, searches, new_folder_title):
     folder_path = os.path.join(os.getcwd(), new_folder_title)
     os.mkdir(folder_path)
 
-    page_count = 0
     for label, search in zip(labels, searches):
         current_path = os.path.join(folder_path, f"{label}")
         os.mkdir(current_path)
         art_num = 0
 
-        search_page = urllib.request.urlopen(search)
+        search_page = urllib.request.urlopen(search, timeout=30)
         soup = BeautifulSoup(search_page, "html.parser")
-        while search_page is not None and page_count < 2:
-            print(f"{page_count * 100} pages out of"
-                  f"{ soup.select_one('div.view-header').getText().split('of')[1].split('records')[0]} processed")
-            page_count += 1
+        total = soup.select_one('div.view-header').getText().split('of')[1].split('records')[0]
+        while search_page is not None and art_num < max_files_each:
             evens = soup.select(".even")
             odds = soup.select(".odd")
             for i in odds:
                 evens.append(i)
-            with io.open(os.path.join(current_path, f"{label}_sample_{art_num}.txt"), 'w', encoding="utf-8"):
-                for article in evens:
-                    # print(f"Processing article #{art_num} of"
-                    #     f"{ soup.select_one('div.view-header').getText().split('of')[1].split('records')[0]}")
-                    f = open(os.path.join(current_path, f"{label}_sample_{art_num}.txt"), 'w', errors="ignore")
-                    f.write(extract_speech(
-                        urllib.request.urlopen("https://www.presidency.ucsb.edu" + article.select('a')[1]['href'])))
-                    art_num += 1
+            for article in evens:
+                if art_num >= max_files_each:
+                    break
+                with io.open(os.path.join(current_path, f"{label}_sample_{art_num}.txt"), 'w', errors="ignore", encoding='utf-8') as f:
+                    while True:
+                        try:
+                            print(f"\rProcessing article {art_num} of {total} in class {label}", end="")
+                            f.write(extract_speech(
+                                urllib.request.urlopen("https://www.presidency.ucsb.edu" + article.select('a')[1]['href'])))
+                            art_num += 1
+                            break
+                        except:
+                            print(f"\rArticle number {art_num} failed, trying again", end="")
+            print(f"\rFinished processing all articles for class {label}")
             try:
                 search_page = urllib.request.urlopen("https://www.presidency.ucsb.edu" +
-                                                     soup.find('a', {'title': 'Go to next page'})['href'])
+                                                     soup.find('a', {'title': 'Go to next page'})['href'], timeout=30)
                 soup = BeautifulSoup(search_page, "html.parser")
             except TypeError:
                 break
+
 
 
 def fetchData(folder_title):
@@ -115,13 +122,16 @@ def fetchData(folder_title):
 
 
 if __name__ == "__main__":
+    '''labels = ["trump", "obama"]
+    searches = [
+        "https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2=&field-keywords3="
+        "&from%5Bdate%5D=&to%5Bdate%5D=&person2=200301&items_per_page=100",
+        "https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2=&field-keywords3="
+        "&from%5Bdate%5D=&to%5Bdate%5D=&person2=200300&items_per_page=100"]
+    writeData(labels, searches, 10_000, "tester")'''
 
-    labels = ["trump", "obama"]
-    searches = ["https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2=&field-keywords3=&from%5Bdate%5D=10-10-2018&to%5Bdate%5D=07-06-2019&person2=200301&category2%5B%5D=&items_per_page=100",
-                "https://www.presidency.ucsb.edu/advanced-search?field-keywords=&field-keywords2=&field-keywords3=&from%5Bdate%5D=10-10-2015&to%5Bdate%5D=07-06-2016&person2=200300&items_per_page=100"]
-    writeData(labels, searches, "small_data")
-    r_labels, r_samples = fetchData("small_data")
-    classLabels = [dirname for dirname in sorted(os.listdir(os.getcwd() + "//small_data"))]
+    r_labels, r_samples = fetchData("trump_obama_corpus")
+    classLabels = [dir_name for dir_name in sorted(os.listdir(os.path.join(os.getcwd(), "trump_obama_corpus")))]
     train_samples, train_labels, val_samples, val_labels, vectorizer = organizeData(r_labels, r_samples, 10, .2)
     embeddingSpace = loadEmbedding(vectorizer, os.path.join(os.getcwd(), "glove.6B", "glove.6B.50d.txt"))
     modelArch = designModel(embeddingSpace, len(classLabels), 10, vectorizer)
