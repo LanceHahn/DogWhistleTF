3a53f340624fdbe10deef71e220f9bc7fbcada5d
desmondharris <desmondktharris@gmail.com>
2022-11-09 13:21:40 -0600

    Update compareEmbeddings funct

--- RetrainEmbeddings.py
+++ RetrainEmbeddings.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from scipy.spatial import distance
 from tensorflow import keras
 from keras import layers
 from PartyClassifier import fetchData
@@ -29,7 +30,7 @@ def retrainEmbeddings(dim, folderName, batchSize, windowSz) -> np.array:
         len(voc) + 2,
         len(standard_embeddings[0]),
         embeddings_initializer=keras.initializers.Constant(standard_embeddings),
-        trainable = False
+        trainable = True
     )
 
     int_input = keras.Input(shape=(None,), dtype="int64")
@@ -71,3 +72,30 @@ def retrainEmbeddings(dim, folderName, batchSize, windowSz) -> np.array:
     final_model = keras.Model(string_layer, preds)
     final_model.summary()
     return embedding_layer.embeddings
+
+
+def compareEmbeddings(untrained, trained):
+    """Function comparing word embedding similarities from two embedding spaces using GenSim
+
+    Args:
+        untrained (Numpy matrix): Standard embedding space
+        trained (Numpy matrix): Retrained/customized embedding space
+    """
+    if len(untrained) != len(trained):
+        raise Exception("Matrix dimensions must be identical")
+
+    untr_dict={}
+    tr_dict = {}
+    with open(os.path.join(os.getcwd(), "glove.6B", "glove.6B.50d.txt")) as f:
+        for line, untr_vect, tr_vect in zip(f, untrained, trained):
+            word = line.split(maxsplit=1)[0]
+        untr_dict[word] = untr_vect
+        tr_dict[word] = tr_vect
+
+
+if __name__ == "__main__":
+    organizeData(fetchData("trump_obama_corpus"))
+    new_embed = retrainEmbeddings(50, "trump_obama_corpus", 128, 5)
+    old_embed = loadEmbedding(organizeData(fetchData("trump_obama_corpus"))[4], os.path.join(os.getcwd(), "glove.6B", "glove.6b.50d.txt"))
+    compareEmbeddings(old_embed, new_embed)
+    
