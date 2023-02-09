6df5a9b8c23ac4aa967add42dbd5d9edcc5311df
desmondharris <desmondktharris@gmail.com>
2023-02-09 12:27:35 -0600

    Add Slate functions and clean up code

--- PoliticalScrapers.py
+++ PoliticalScrapers.py
@@ -7,15 +7,6 @@ class Scraper:
     def __init__(self):
         self.pages_accessed = 0
 
-    def msnbc_from_link(self, link: str) -> str:
-        page = urllib.request.urlopen(link) 
-        soup = BeautifulSoup(page, "lxml")
-        paragraphs = soup.select(".showblog-body__content p")
-        full_text = ""
-        for p in paragraphs:
-            full_text += p.getText()
-        return full_text
-
     def fox_from_link(self, link: str) -> str: 
         page = urllib.request.urlopen(link)
         soup = BeautifulSoup(page, "html.parser")
@@ -28,18 +19,38 @@ class Scraper:
     def slate_from_link(self, link: str) -> str: 
         page = urllib.request.urlopen(link)
         soup = BeautifulSoup(page, "html.parser")
-        paragraphs = soup.select(".slate-paragraph slate-graf")
+        paragraphs = soup.select(".slate-paragraph.slate-graf")
         body_text = ""
         for p in paragraphs:
             body_text += p.getText()
         return body_text
 
+    def slate_n_articles(self, link: str, n : int) -> list:
+        texts = []
+        for _ in range(n):
+            page = urllib.request.urlopen(link)
+            soup = BeautifulSoup(page, features="lxml")
+            texts.append(self.article_from_link(link))
+
+            next_article = soup.select("a.in-article-recirc__link")[0]
+            link = next_article['href']
+        return texts
+
+
+            
+    def msnbc_from_link(self, link: str) -> str:
+        page = urllib.request.urlopen(link) 
+        soup = BeautifulSoup(page, "lxml")
+        paragraphs = soup.select(".showblog-body__content p")
+        full_text = ""
+        for p in paragraphs:
+            full_text += p.getText()
+        return full_text
 
     def msnbc_n_articles(self, link: str, n: int) -> list:
         texts = []
         for _ in range(n):
             page = urllib.request.urlopen(link)
-            self.pages_accessed += 1
             soup = BeautifulSoup(page, features="lxml")
             texts.append(self.article_from_link(link))
 
@@ -48,20 +59,6 @@ class Scraper:
             link = next_button['href']
         return texts
 
-
-    def article_from_link(self, link):
-        site = link.replace("https://", "")
-        site = site.replace("www.", "")
-        site = site[:3]
-        function_dict = {
-            "msn" : self.msnbc_from_link,
-            "fox" : self.fox_from_link,
-            "new": self.newsmax_from_link 
-        }
-
-        self.pages_accessed += 1
-
-        return function_dict[site](link)
     
     def newsmax_from_link(self, link: str) -> str:
         page = urllib.request.urlopen(link)
@@ -75,7 +72,6 @@ class Scraper:
     def newsmax_n_articles(self, link: str, n: int) -> str:
         texts = []
         for i in range(n):
-            self.pages_accessed += 1
             page = urllib.request.urlopen(link)
             soup = BeautifulSoup(page, "html.parser")
             texts.append(self.article_from_link(link))
@@ -84,8 +80,20 @@ class Scraper:
             link = "https://www.newsmax.com" + next_page_link['href']
         return texts
 
-        
+    def article_from_link(self, link):
+        site = link.replace("https://", "")
+        site = site.replace("www.", "")
+        site = site[:3]
+        function_dict = {
+            "msn" : self.msnbc_from_link,
+            "fox" : self.fox_from_link,
+            "new": self.newsmax_from_link,
+            "sla": self.slate_from_link
+        }
 
+        self.pages_accessed += 1
+
+        return function_dict[site](link)
     def n_articles(self, link):
         site = link.rsplit("www.", 1)[1][:3]
         function_dict = {
@@ -100,4 +108,4 @@ class Scraper:
 
 
 test = Scraper()
-print(test.newsmax_n_articles("https://www.newsmax.com/us/china-spy-balloon/2023/02/04/id/1107311/", 3)[0])
+print(test.slate_n_articles("https://slate.com/technology/2023/02/twitter-api-bots-elon-musk.html", 3))
