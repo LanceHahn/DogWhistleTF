56af22f95eeba112e78a6d2551a936b69f13b01f
desmondharris <desmondktharris@gmail.com>
2023-03-05 19:14:04 -0600

    for pull

--- preTrainedSample.py
+++ preTrainedSample.py
@@ -4,7 +4,9 @@ https://keras.io/examples/nlp/pretrained_word_embeddings/
 """
 
 import numpy as np
+
 import tensorflow as tf
+tf.get_logger().setLevel('INFO')
 from tensorflow import keras
 from keras import layers
 from keras.layers import TextVectorization
